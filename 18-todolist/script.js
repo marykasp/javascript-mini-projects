739def964acf6056afc1ef637e63ad762bb2e594
marykasp <kasparian.mary@gmail.com>
2024-01-22 13:23:47 -0600

    grab all the dom elements needed to create new tasks

---
+++ 18-todolist/script.js
@@ -0,0 +1,7 @@
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#form-wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.querySelector("#error");
+const countValue = document.querySelector(".count");
+
+let taskCount = 0;
