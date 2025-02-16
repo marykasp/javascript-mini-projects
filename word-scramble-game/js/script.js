a5d0d6ab9aa221d4670949c06231201d31bc8595
marykasp <kasparian.mary@gmail.com>
2025-02-16 11:04:24 -0600

    Check word function, compares user word with correct word and display different alerts

--- word-scramble-game/js/script.js
+++ word-scramble-game/js/script.js
@@ -1,10 +1,25 @@
 const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkBtn = document.querySelector(".check-word");
+const inputField = document.querySelector("input");
+const warning = document.querySelector(".warning");
+
+let correctWord;
 
 const initGame = () => {
+  //clear input field
+  inputField.value = "";
+
+  // remove any text from the warning p element
+  warning.textContent = "";
+
   // get random word object from words list
   let randomObj = words[Math.floor(Math.random() * words.length)];
   // split each letter of random word
   let wordArray = randomObj.word.split("");
+  // save correct word to variable
+  correctWord = randomObj.word.toLocaleLowerCase();
 
   // iterate over each letter in wordArray to shuffle the letters around
   for (let i = wordArray.length - 1; i > 0; i--) {
@@ -15,9 +30,43 @@ const initGame = () => {
     [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
   }
 
-  wordText.innerText = wordArray.join("");
+  wordText.innerText = wordArray.join(""); // passing shuffled word as text to p element
+  hintText.innerText = randomObj.hint; // passing word hint as text to element
+
+  inputField.setAttribute("maxlength", correctWord.length);
+
+  console.log(correctWord);
   console.log(wordArray, randomObj.word);
   console.log(randomObj);
 };
 
 initGame();
+
+const clearText = () => {
+  // clear input field
+  inputField.value = "";
+};
+
+const checkWord = () => {
+  let userWord = inputField.value.toLocaleLowerCase();
+  console.log(userWord);
+
+  //if no word entered into input create a warning element to display under input
+  if (!userWord) {
+    warning.textContent = "Please enter a word";
+    console.dir(warning);
+    inputField.insertAdjacentElement("afterend", warning);
+    return;
+  }
+  // check if user word is the same as the correct word
+  if (userWord !== correctWord) {
+    warning.textContent = "";
+    return alert(`Oops! ${userWord} is incorrect`);
+  }
+
+  alert(`Congrats ${userWord.toUpperCase()} is correct!`);
+  initGame();
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
