9ae08b1a8ce8e79eb5472f2bfa29f82ae060d316
marykasp <kasparian.mary@gmail.com>
2025-02-19 08:00:00 -0600

    Generate themed card, download card as an image

--- greeting-card-maker/script.js
+++ greeting-card-maker/script.js
@@ -1,4 +1,5 @@
 const generateCardBtn = document.getElementById("generateCard");
+const downloadCardBtn = document.getElementById("downloadCard");
 
 generateCardBtn.addEventListener("click", () => {
   // get value of both inputs
@@ -13,4 +14,32 @@ generateCardBtn.addEventListener("click", () => {
   }
 
   let cardTitle = document.getElementById("cardTitle");
+  let cardMessage = document.getElementById("cardMessage");
+  let card = document.getElementById("card");
+
+  let themeMessages = {
+    birthday: `🥮 Happy Birthday, ${name}! Wishing you a fantastic day!`,
+    congratulations: `Congratulations, ${name}!🏆 Keep shinning and achieving great things!`,
+    love: `💛 Dear ${name}, you are truly special! Sending love your way 💘`,
+    friendship: `Hey ${name}! You are an amazing friend! Stay awesome`,
+  };
+
+  // create title with uppercase first letter from theme option input
+  cardTitle.innerText = `${
+    theme.charAt(0).toUpperCase() + theme.slice(1)
+  } Greeting`;
+
+  cardMessage.innerText = themeMessages[theme];
+
+  card.style.display = "block";
+});
+
+// download Card as an Image
+downloadCardBtn.addEventListener("click", () => {
+  html2canvas(document.getElementById("card")).then((canvas) => {
+    let link = (document = document.createElement("a"));
+    link.href = canvas.toDataURL("image/png");
+    link.download = "greeting_card.png";
+    link.click();
+  });
 });
