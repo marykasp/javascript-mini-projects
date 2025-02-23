c85ab2d1b1476b4e84ae7f77def6b08d682e9086
marykasp <kasparian.mary@gmail.com>
2025-02-23 13:21:34 -0600

    Create a line chart to display average GPA vs. class GPA

--- chart-dashboard/script.js
+++ chart-dashboard/script.js
@@ -1,4 +1,5 @@
 const ctx = document.querySelector(".activity-chart");
+const ctx2 = document.querySelector(".prog-chart");
 
 new Chart(ctx, {
   type: "bar",
@@ -31,6 +32,56 @@ new Chart(ctx, {
       y: {
         ticks: {
           display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    animation: {
+      duration: 1000,
+      easing: "easeInOutQuad",
+    },
+  },
+});
+
+new Chart(ctx2, {
+  type: "line",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    datasets: [
+      {
+        label: "Class GPA",
+        data: [6, 10, 8, 14, 6, 7, 4],
+        borderColor: "#0891b2",
+        tension: 0.4,
+      },
+      {
+        label: "Aver GPA",
+        data: [8, 6, 7, 6, 11, 8, 10],
+        borderColor: "#ca8a04",
+        tension: 0.4,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        ticks: {
+          display: false,
+        },
+        border: {
+          display: false,
+          dash: [5, 5],
         },
       },
     },
